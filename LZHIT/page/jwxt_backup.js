8231384877ff680f71fbf5791621655907d74fe5
IveTian <ivetien@outlook.com>
2021-09-17 21:19:04 +0800

    update

--- LZHIT/page/jwxt_backup.js
+++ LZHIT/page/jwxt_backup.js
@@ -1,10 +1,24 @@
 import React, { useState } from "react";
-import { SafeAreaView, StyleSheet, View, StatusBar, TouchableOpacity, Image } from 'react-native';
+import { SafeAreaView, StyleSheet, Linking, View, StatusBar, TouchableOpacity, Image } from 'react-native';
 import { WebView } from 'react-native-webview';
 
 const statusBarHeight = StatusBar.currentHeight;
 
-const netWork = () => {
+_injectJavaScript = () => `
+var a = document.getElementsByTagName('a');
+for(var i = 0; i < a.length; i++){
+    a[i].onclick = function (event) {
+        window.postMessage(this.href);
+        event.preventDefault();
+    }
+}
+`
+
+_onMessage = (e) => {
+Linking.openURL(e.nativeEvent.data).catch(err => console.error('An error occurred', err))
+}
+
+const Jwxt = () => {
     const [url, setUrl] = useState('http://jwglxt.lzhit.edu.cn/jwglxt/xtgl/');
     const [webView, setWebview] = useState(null);
     const [urlInput, setUrlInput] = useState(url);
@@ -20,48 +34,30 @@ const netWork = () => {
     };
 
     //接收web发送过来的信息
-    const onMessage = (event) => {
-        const rep = event.nativeEvent.data;
-        //console.log('-----------webview返回结果--------------');
-        let minLog = rep;
-        if (rep.length > 300) {
-            minLog = rep.substring(0, 290);    //日志太长影响观感
-        }
-        console.log(minLog);
-    };
     return (
         <SafeAreaView style={{ flex: 1, marginTop: statusBarHeight }}>
             <WebView source={{ uri: url }}
-                injectedJavaScript={`
-                window.onload = function() {
-                    var tags = document.getElementsByTagName("a");
-                    for(var i = 0; i < tags.length; i++) {
-                        tags[i].addEventListener('click', function (e) {
-                            if(e.href.indexOf("forward.action") != -1) e.preventDefault();
-                        })
-                    };
-                }
-            `}
                 ref={(webView) => (setWebview(webView))}
                 sharedCookiesEnabled={true}
                 startInLoadingState={true}
                 onLoad={webViewOnLoad}
-                onMessage={onMessage}
+                onMessage={this._onMessage}
+                injectedJavaScript={this._injectJavaScript}
                 onError={syntheticEvent => {
                     const { nativeEvent } = syntheticEvent;
-                    console.log('校园网登陆页面加载失败！请确认是否已连上校园网 WiFi。');
+                    console.log('网络连接失败！');
                     console.warn('WebView error: ', nativeEvent);
                 }}
             />
             <View style={{ flexDirection: 'row', justifyContent: 'space-between', height: 60, backgroundColor: 'white' }}>
                 <TouchableOpacity onPress={() => { webView.goBack(); }}>
-                <Image source={require('../resource/images/goBack.png')} style={{width:40,height:40,marginTop:10,marginBottom:10,marginLeft:40}}/>
+                    <Image source={require('../resource/images/goBack.png')} style={{ width: 40, height: 40, marginTop: 10, marginBottom: 10, marginLeft: 40 }} />
                 </TouchableOpacity>
                 <TouchableOpacity onPress={() => { webView.reload(); }}>
-                <Image source={require('../resource/images/refresh.png')} style={{width:40,height:40,marginTop:10,marginBottom:10,}}/>
+                    <Image source={require('../resource/images/refresh.png')} style={{ width: 40, height: 40, marginTop: 10, marginBottom: 10, }} />
                 </TouchableOpacity>
                 <TouchableOpacity onPress={() => { webView.goForward(); }}>
-                <Image source={require('../resource/images/goForward.png')} style={{width:40,height:40,marginTop:10,marginBottom:10,marginRight:40}}/>
+                    <Image source={require('../resource/images/goForward.png')} style={{ width: 40, height: 40, marginTop: 10, marginBottom: 10, marginRight: 40 }} />
                 </TouchableOpacity>
             </View>
 
@@ -71,4 +67,4 @@ const netWork = () => {
 const styles = StyleSheet.create({
 });
 
-export default netWork;
+export default Jwxt;
